da293b818f3395b0a016577e7762f420d0bebe33
Eli Ribble <eli@theribbles.org>
2024-08-22 17:30:23 -0700

    Capture stderr and status code.

--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
+	
 	cmd := exec.Command("journalctl", "-u", "systemd-networkd", "-p", "6", "-g", "DHCP: received delegated prefix")
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("Error: ", err);
-		return
+		if exitError, ok := err.(*exec.ExitError); ok {
+			exitCode := exitError.ExitCode()
+			fmt.Println("Exit code:", exitCode)
+			os.Exit(exitCode)
+		} else {
+			fmt.Println("Command failed with:", err)
+			fmt.Println("Captured stderr:", stderr.String())
+			fmt.Println("Captured stdout:", out.String())
+			os.Exit(100)
+		}
 	}
 	output := out.String()
 
