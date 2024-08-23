30461dfac211fc40b70b7ee9f9f2a93ee70483df
Eli Ribble <eli@theribbles.org>
2024-08-22 17:22:53 -0700

    Run the journalctl process.

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	cmd := exec.Command("journalctl", "-u", "systemd-networkd", "-p", "6", "-g", "DHCP: received delegated prefix")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error: ", err);
+		return
+	}
+	output := out.String()
+
+	fmt.Println("Captured output: ")
+	fmt.Println(output)
 }
