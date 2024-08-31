40abcb3c3e52f8af5673517f6f2e22e24d39d7ef
Eli Ribble <eli@theribbles.org>
2024-08-30 17:14:07 -0700

    Re-connect to sqlite, return a DB connection.
    Also start using the regular logger more instead of just printing to
    stdout like a pleb

--- daemon.go
+++ daemon.go
@@ -3,30 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/tealok-tech/tealok/database"
-	"github.com/tealok-tech/tealok/log"
+	slog "github.com/tealok-tech/tealok/log"
 	"github.com/tealok-tech/tealok/networkd"
 	"github.com/tealok-tech/tealok/podman"
 	"github.com/tealok-tech/tealok/server"
+	"log"
 	"os"
 )
 
 func main() {
 	// Set up the structured log
-	log.Setup()
+	slog.Setup()
 	// Open the local database
-	database.Connect()
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal("Unable to connect to the database", err)
+		os.Exit(1)
+	}
 	// Get the correct subnet from the log
 	_, subnet, err := networkd.Subnet()
 	if err != nil {
 		fmt.Println("Failed to get subnet", err)
-		os.Exit(1)
+		os.Exit(2)
 	}
 	fmt.Println("Subnet should be", subnet.IP, subnet.Mask.String())
 
 	// Get or create a network with the correct subnet
 	containerNetwork, err := podman.EnsureNetworkWithSubnet(subnet.IP)
 	if err != nil {
-		os.Exit(2)
+		os.Exit(3)
 	}
 	fmt.Println("Tealok network name:", containerNetwork.Name)
 
--- database/database.go
+++ database/database.go
@@ -1,27 +1,45 @@
 package database
 
 import (
-	"fmt"
-	"log"
-
+	"database/sql"
 	migrate "github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/source/go_bindata"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
 
 	_ "github.com/golang-migrate/migrate/v4/database/sqlite"
 	"github.com/tealok-tech/tealok/database/migrations"
 )
 
-func Connect() {
-	fmt.Println("Connecting to database")
+const DB_FILE = "/var/lib/tealok/database.sqlite"
+
+func Connect() (*sql.DB, error) {
+	log.Println("Connecting to database")
+	// Connect up and run any migrations that are outstanding
 	s := bindata.Resource(migrations.AssetNames(),
 		func(name string) ([]byte, error) {
 			return migrations.Asset(name)
 		})
 	d, err := bindata.WithInstance(s)
-	m, err := migrate.NewWithSourceInstance("go-bindata", d, "sqlite:///var/lib/tealok/database.sqlite")
+	m, err := migrate.NewWithSourceInstance("go-bindata", d, "sqlite://"+DB_FILE)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return nil, err
 	}
 	m.Up()
+	m.Close()
+
+	// Geta new connection where we can make queries
+	db, err := sql.Open("sqlite3", DB_FILE)
+	if err != nil {
+		return nil, err
+	}
+
+	var version string
+	row := db.QueryRow("select sqlite_version()")
+	err = row.Scan(&version)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("SQLite version:", version)
+	return db, nil
 }
