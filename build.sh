4bfcf24d0b97ace404dc7d80d07ed2ee5ad66978
Eli Ribble <eli@theribbles.org>
2024-09-06 10:56:37 -0700

    Show a bit of progress when building.

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ popd
 pushd database/migrations
 go-bindata -pkg migrations .
 popd
+echo "Building daemon"
 go build -o ./tmp/tealok-daemon daemon.go
+echo "Building client"
 go build -o ./tmp/tealok-client client.go
